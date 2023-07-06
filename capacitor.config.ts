0aec4a74d7f7983efe04ba6685115e55278b83bb
ThiagoHBarros <thiagohenrique.b@live.com>
2023-07-06 18:09:36 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Som dos Animais',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
